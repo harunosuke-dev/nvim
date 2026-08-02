5686e1608a9c30fa2f48d286f97459f76e1905fa
Harunosuke <harunosuke.dev@gmail.com>
2026-08-03 02:27:12 +0900

    docs(keymap): which-key のグループ名に由来の英単語を添える
    キーがどの英単語の頭文字から来ているかが分かると、忘れても思い出せる。
    f が Find だと分かれば ff = Find File と辿れる。

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -8,19 +8,21 @@ return {
     -- timeoutlen (150) より小さくする。ポップアップが出る前に
     -- キー列がタイムアウトすると <leader> 操作が中断されてしまう
     delay = 100,
+    -- グループ名には由来の英単語を添える。どの頭文字から来ているかが分かると
+    -- 覚え直さずに思い出せる（例: f は Find なので ff = Find File）
     spec = {
-      { '<leader>a', group = '引数（Treesitter）' },
-      { '<leader>b', group = 'バッファ' },
-      { '<leader>c', group = 'コード' },
-      { '<leader>f', group = '検索（fzf-lua）' },
-      { '<leader>g', group = 'Git / LSP ジャンプ' },
-      { '<leader>h', group = 'Git hunk' },
-      { '<leader>n', group = '通知・メッセージ' },
-      { '<leader>r', group = '検索・置換' },
-      { '<leader>s', group = 'セッション' },
-      { '<leader>sn', group = 'npm パッケージ' },
-      { '<leader>u', group = '表示の切り替え' },
-      { '<leader>x', group = '一覧（trouble）' },
+      { '<leader>a', group = 'Argument 引数' },
+      { '<leader>b', group = 'Buffer バッファ' },
+      { '<leader>c', group = 'Code コード' },
+      { '<leader>f', group = 'Find 探す' },
+      { '<leader>g', group = 'Go to 移動 / Git' },
+      { '<leader>h', group = 'Hunk 変更のかたまり' },
+      { '<leader>n', group = 'Notification 通知' },
+      { '<leader>r', group = 'Replace 置換' },
+      { '<leader>s', group = 'Session セッション' },
+      { '<leader>sn', group = 'NPM パッケージ' },
+      { '<leader>u', group = 'UI 表示の切り替え' },
+      { '<leader>x', group = '一覧（trouble の慣例）' },
       { ']', group = '次へ' },
       { '[', group = '前へ' },
     },
